36d43d6268fa283ef1f71cd76d6c9a282c89366c
ondry <ondryas5@seznam.cz>
2016-01-13 23:11:13 +0100

    new view update

--- app/views/error/404.php
+++ app/views/error/404.php
@@ -1,7 +1,7 @@
 <!-- 404 error -->
 <section id="error" class="container">
     <h1>404, Stránka nenalezena</h1>
-    <p>Stránka, kterou hledáte, neexistuje. Pravděpodobně jste při zadávání URL adresy udělali chybu.lol</p>
+    <p>Stránka, kterou hledáte, neexistuje. Pravděpodobně jste při zadávání URL adresy udělali chybu.lola</p>
     <a class="btn btn-success" href="<?= URL ?>">ZPĚT NA HLAVNÍ STRÁNKU</a>
 </section>
 <!-- /404 error -->
